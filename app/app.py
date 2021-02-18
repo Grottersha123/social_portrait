b89e89b238cc495656a22f561169026ad8bfac49
ZeroOne <grottersha123@gmail.com>
2021-02-18 12:50:54 +0300

    add  invalid Id

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
+import vk_api
 
 from vk_coomon.vk_scrap import ScrapVk
 from common.classificator_sentiment_analysis import multiply_array
@@ -25,18 +26,22 @@ class GetSentimentVk(Resource):
 
             user_id, post_count = args['user_id'], args['post_count']
             vk_scrap = ScrapVk(VK_TOKEN)
-            info_data = vk_scrap.get_info(user_id=user_id)
-            if info_data and info_data[0]['can_see_all_posts']:
-                info_data = info_data[0]
+            try:
+                info_data = vk_scrap.get_info(user_id=user_id)
+                if info_data and info_data[0]['can_see_all_posts']:
+                    info_data = info_data[0]
 
-                user_id = info_data['id']
-                posts, likes, count_posts, post_dict = vk_scrap.get_post_by_date(user_id=user_id, COUNT=int(post_count))
-                info_data['count_posts'] = count_posts
-                info_data['count_likes'] = likes
-                sentiment_data_posts, sentiment_data_binary, sentiment_data_table = multiply_array(posts, post_dict)
-                return {'data_info': info_data, 'data_posts': sentiment_data_posts,
-                        'data_binary': sentiment_data_binary, 'data_table': sentiment_data_table}
-            return {'data_info': info_data[0], 'data_posts': None, 'data_binary': None, 'data_table': None}
+                    user_id = info_data['id']
+                    posts, likes, count_posts, post_dict = vk_scrap.get_post_by_date(user_id=user_id, COUNT=int(post_count))
+                    info_data['count_posts'] = count_posts
+                    info_data['count_likes'] = likes
+                    sentiment_data_posts, sentiment_data_binary, sentiment_data_table = multiply_array(posts, post_dict)
+                    return {'data_info': info_data, 'data_posts': sentiment_data_posts,
+                            'data_binary': sentiment_data_binary, 'data_table': sentiment_data_table}
+                return {'data_info': info_data[0], 'data_posts': None, 'data_binary': None, 'data_table': None}
+            except vk_api.exceptions.ApiError:
+                return {'error': 'Invalid Id'}, 404
+
         else:
             return {'error': 'bad request'}, 404
 
